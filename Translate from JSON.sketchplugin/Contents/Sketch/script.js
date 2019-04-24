f48c314dd01c294590ed8f2d1ca74e26826abb42
Kurtulus Birgoren <kbirgoren@gmail.com>
2019-04-24 15:37:53 +0200

    Update script.js

--- Translate from JSON.sketchplugin/Contents/Sketch/script.js
+++ Translate from JSON.sketchplugin/Contents/Sketch/script.js
@@ -25,100 +25,75 @@ var checkRegex = function(string){
     else return false;
 }
 
-var importFile = function(context) {
-
-  sketch.UI.message('Translate from JSON')
-
-  var contextApi = context.api();
-  var languageFile = null;
-  var languageData = null;
-  var openPanel = NSOpenPanel.openPanel();
-
-  openPanel.setCanChooseFiles(true)
-  openPanel.setCanChooseDirectories(false)
-  openPanel.setAllowsMultipleSelection(false)
-  openPanel.setAllowedFileTypes(["json"])
-
-  var openPanelButtonPressed = openPanel.runModal()
-
-  if (openPanelButtonPressed == NSFileHandlingPanelOKButton) {
-    languageFile = openPanel.URL()
-  } else {
-    sketch.UI.message('Cancelled')
-  }
-
-  var fileContent = JSON.parse(NSString.stringWithContentsOfFile(languageFile))
-
-  saveLanguageFile(stringify(fileContent), context.scriptPath);
-  updateLanguage(context)
-
-}
-
-
-var updateLanguage = function(context) {
-
-  sketch.UI.message('Updating...')
-
-  var contextApi = context.api();
-  var document = sketch.getSelectedDocument();
-  var selectedLanguage = null;
-  var languageData = readLanguageFile(context.scriptPath);
-  var availableLanguages = Object.keys(languageData);
-  var dropdown = contextApi.getSelectionFromUser("Choose A Language", availableLanguages, 0);
-
-  dropdownSelectedIndex = dropdown[1];
-  selectedLanguage = availableLanguages[dropdownSelectedIndex];
-
-  document.pages.forEach(translate)
-
-  function translate(object) {
-
-    object.layers.forEach(artboard => {
-
-      artboard.layers.forEach(layer => {
-
-        if (layer.type === 'Text') {
-
-          variableName = layer.name
-
-          if (checkRegex(variableName)) {
-
-            sketch.UI.message('Translating:' + variableName)
-            translation = eval('languageData.' + selectedLanguage + '.' + checkRegex(variableName) + '')
-
-            if (translation) {
-              layer.text = translation;
-            } else {
-              layer.text = 'ERROR:' + variableName + '';
-            }
-
-            layer.name = variableName;
-          }
-        }
-
-        if (layer.type === 'SymbolInstance') {
-          layer.overrides.forEach(override => {
-            variableName = override.affectedLayer.name
-            if (checkRegex(variableName)) {
-              translation = eval('languageData.' + selectedLanguage + '.' + checkRegex(variableName) + '')
-              if (override.affectedLayer.type === 'Text') {
-                if (translation) {
-                  override.value = translation;
-                } else {
-                  override.value = 'ERROR:' + variableName + '';
-                }
-              }
-            }
-
-          })
-        }
-
-      })
-
-    })
-
-    sketch.UI.message('Translated')
+var importFile = function (context) {
     
-  }
+    sketch.UI.message('Translate from JSON')
+    
+    var contextApi = context.api();
+    var languageFile = null;
+    var languageData = null;
+    
+    var openPanel = NSOpenPanel.openPanel();
+    openPanel.setCanChooseFiles(true)
+    openPanel.setCanChooseDirectories(false)
+    openPanel.setAllowsMultipleSelection(false)
+    openPanel.setAllowedFileTypes(["json"])
+    openPanelButtonPressed = openPanel.runModal()
+    
+    if (openPanelButtonPressed == NSFileHandlingPanelOKButton) {
+        languageFile = openPanel.URL()
+    } else {
+        sketch.UI.message('Cancelled')
+    }
 
+    var fileContent = JSON.parse(NSString.stringWithContentsOfFile(languageFile))
+    saveLanguageFile(stringify(fileContent), context.scriptPath);
+    updateLanguage(context)
+}
+var updateLanguage = function (context) {
+    
+    sketch.UI.message('Updating...')
+    
+    var contextApi = context.api();
+    var document = sketch.getSelectedDocument();
+    var selectedLanguage = null;
+    var languageData = readLanguageFile(context.scriptPath);
+    var availableLanguages = Object.keys(languageData);
+    var dropdown = contextApi.getSelectionFromUser("Choose A Language", availableLanguages, 0);
+    
+    dropdownSelectedIndex = dropdown[1];
+    selectedLanguage = availableLanguages[dropdownSelectedIndex];
+    document.pages.forEach(translate)
+
+    function translate(object) {
+        object.layers.forEach(artboard => {
+            artboard.layers.forEach(layer => {
+                
+                if (layer.type === 'Text') {
+                    variableName = layer.name
+                    if (checkRegex(variableName)) {
+                        sketch.UI.message('Translating:' + variableName)
+                        translation = eval('languageData.' + selectedLanguage + '.' + checkRegex(variableName) + '')
+                        if (translation) layer.text = translation;
+                        else layer.text = 'ERROR:' + variableName + '';
+                        layer.name = variableName;
+                    }
+                }
+               
+                if (layer.type === 'SymbolInstance') {
+                    layer.overrides.forEach(override => {
+                        variableName = override.affectedLayer.name
+                        if (checkRegex(variableName)) {
+                            translation = eval('languageData.' + selectedLanguage + '.' + checkRegex(variableName) + '')
+                            if (override.affectedLayer.type === 'Text') {
+                                if (translation) override.value = translation;
+                                else override.value = 'ERROR:' + variableName + '';
+                            }
+                        }
+                    })
+                }
+            })
+        })
+        sketch.UI.message('Translated')
+    }
 }
